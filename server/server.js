75c823e4a7709d89abab2399f2b311955c207632
Harsh.Shah <Harsh.shah@stem.com>
2020-08-24 17:54:50 +0530

    socket connection at server & client end

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const path  = require('path');
+const http = require('http');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const SocketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, ".." , "public");
 
+const server = http.createServer(app);
+var io = SocketIO(server);
+
 app.use(express.static(publicPath))
 
-
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('disconnected from server to client')
+})
+  console.log(`new user connected`)});
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
   
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
