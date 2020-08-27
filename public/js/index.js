0e1abe51391d9b6fe89544ea97ff8e73b0cddd20
Harsh.Shah <Harsh.shah@stem.com>
2020-08-27 20:02:24 +0530

    moment

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 var socket = io();
+// var moment = require('./libs/moment')
 
 socket.on('connect', function () {
     console.log('connected to server from client')
@@ -7,6 +8,8 @@ socket.on('connect', function () {
         from: 'abc@email.com',
         to: '12@gmail.com',
         text: 'hi'
+    }, (message) => {
+        console.log(message);
     })
 });
 
@@ -21,3 +24,41 @@ socket.on('newMessage', function (obj) {
 socket.on('disconnect',function () {
     console.log('disconnected to server from client')
 });
+
+
+jQuery('#message-form').on('submit', function (e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:"user",
+        text: jQuery('[name=message]').val()
+    },(obj) => {
+        console.log(`${obj.from}: ${obj.text}`)
+        var li = jQuery('<li></li>');
+        li.text=`${obj.from}: ${obj.text}`
+        jQuery('#messages').append(`<li>${obj.from} ${moment(obj.createdAt).format('hh:mm a')}: ${obj.text}</li>`);
+    })
+    console.log('createmessage')
+})
+
+const locBtn = jQuery('#send-location');
+locBtn.on('click', function (e){
+    e.preventDefault();
+    if(!navigator.geolocation) {
+        alert('Geolocation is not supported by your browser');
+      } else {
+        locBtn.attr('disabled','disabled').text("Send Location...")
+        navigator.geolocation.getCurrentPosition(function (position) {
+            socket.emit('createLocationMessage', {
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+            }, (message) => {
+                locBtn.removeAttr('disabled')
+                jQuery('#messages').append(`<li>${message.from} ${moment(message.createdAt).format('hh:mm a')}: We will sync with u at lat-${message.lat}  long-${message.long}<br> <a target="_blank" href="https://www.google.com/maps?q=${message.lat},${message.long}">my location</a></li>`);
+            })
+        }, function (error) {
+            locBtn.removeAttr('disable')
+            console.log(error)
+            alert('Geolocation is not supported by your browser');
+        });
+      }
+})
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const path  = require('path');
 const http = require('http');
 const express = require('express')
+const moment = require('moment')
 const app = express()
 const port = process.env.PORT || 3000
 const SocketIO = require('socket.io')
-
-const { generateMessage } = './utils/message.js';
+// var jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+// const { window } = new JSDOM();
+// const jQuery = require('jquery')(process.window);
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, ".." , "public");
 
@@ -18,14 +22,23 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('disconnected from server to client')
   })
-  socket.on('createEmail', (obj) => {
+  socket.on('createEmail', (obj, callback) => {
     console.log(obj)
+    callback(`Got it dear ${obj.from}`)
   })
 
-  socket.on('createMessage', (obj) => {
+  socket.on('createMessage', (obj,callback) => {
     console.log(obj)
     // io.emit('newMessage',obj)
-    socket.broadcast.emit('newMessage',obj)
+    socket.broadcast.emit('newMessage',{...obj, createdAt: moment().valueOf()})
+    callback(obj);
+  })
+
+  socket.on('createLocationMessage', (obj,callback) => {
+    console.log(obj)
+    // io.emit('newMessage',obj)
+    socket.broadcast.emit('newMessage',{...obj, from: 'Admin', createdAt: moment().valueOf()})
+    callback({...obj, from: 'Admin'});
   })
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to my place'));
